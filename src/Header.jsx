846620bfb3b3aa7820f8af8cd519e5d477db8d17
Nuriye Yavuz <nuriyeyavuz89@gmail.com>
2024-10-31 17:27:28 +0300

    first commit

---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AssigmentIcon from '@mui/icons-material/Assignment';
+export default function Header() {
+  return (
+   <header>
+    <h1>
+        <AssigmentIcon/>
+        To Do Uygulaması
+    </h1>
+   </header>
+  )
+}
